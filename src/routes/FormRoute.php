d67094f5aad579501d1b6343b5492d4e301539e1
informaticaxos <informaticaxos@gmail.com>
2025-10-12 00:36:55 -0500

    x

--- src/routes/FormRoute.php
+++ src/routes/FormRoute.php
@@ -15,31 +15,3 @@ $routes = [
     'DELETE /forms/{id}' => ['FormController', 'delete'],
 ];
 
-// Función para obtener el método y path de la solicitud
-function getRequestMethodAndPath()
-{
-    $method = $_SERVER['REQUEST_METHOD'];
-    $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    // Remover el prefijo del subdirectorio
-    $path = str_replace('/php-ferco-files-ws/index.php', '', $path);
-    return [$method, $path];
-}
-
-// Función para hacer match de ruta
-function matchRoute($routes, $method, $path)
-{
-    foreach ($routes as $route => $handler) {
-        list($routeMethod, $routePath) = explode(' ', $route, 2);
-        if ($routeMethod !== $method) {
-            continue;
-        }
-
-        // Reemplazar {id} con regex
-        $routePathRegex = preg_replace('/\{[^}]+\}/', '([^/]+)', $routePath);
-        if (preg_match("#^$routePathRegex$#", $path, $matches)) {
-            array_shift($matches); // Remover el match completo
-            return [$handler, $matches];
-        }
-    }
-    return null;
-}
