81bf45be73a314c8896e7a111713c64082bb75e2
informaticaxos <informaticaxos@gmail.com>
2025-10-18 13:19:16 -0500

    EDICION DE RUTA

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ function getRequestMethodAndPath()
     $method = $_SERVER['REQUEST_METHOD'];
     $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     // Remover el prefijo del subdirectorio
-    $path = str_replace('/php-ferco-files-ws/index.php', '', $path);
+    $path = str_replace('/php-ferco-files-ws', '', $path);
     return [$method, $path];
 }
 
